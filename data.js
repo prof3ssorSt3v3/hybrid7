f7dfa0163ef32543dc717eefcc512c8c9799c826
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-08-25 21:04:37 -0400

    added data file

---
+++ data.js
@@ -0,0 +1,20 @@
+/**
+ * Hybrid 7 JS file
+ * Name: Your name here
+ */
+
+const data = {
+  name: 'Your name here',
+  characters: [
+    { id: 1, name: 'Frodo', race: 'Hobbit' },
+    { id: 1, name: 'Gandalf', race: 'Wizard' },
+    { id: 1, name: 'Gimli', race: 'Dwarf' },
+    { id: 1, name: 'Legolas', race: 'Elf' },
+    { id: 1, name: 'Boromir', race: 'Human' },
+    { id: 1, name: 'TreeBeard', race: 'Ent' },
+    { id: 1, name: 'Gollum', race: 'Stoor Hobbit' },
+    { id: 1, name: 'Azog', race: 'Orc' },
+    { id: 1, name: 'Beorn', race: 'Shapeshifter' },
+    { id: 1, name: 'Shelob', race: 'Spider' },
+  ],
+};
